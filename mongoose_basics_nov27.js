f8be98113f957463927acfdb0e151e63de7b28ae
Tatiane <30663303+tatianepinto@users.noreply.github.com>
2020-11-27 11:48:22 -0500

    Create mongoose_basics_nov27.js

---
+++ mongoose_basics_nov27.js
@@ -0,0 +1,84 @@
+var mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+
+var Author = require('./author');
+var Book = require('./book');
+
+mongoose.connect('mongodb://localhost/mongoose_basics', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+   if (err) throw err;
+
+   console.log('Successfully connected');
+
+   // var jamieAuthor = new Author({
+   //    _id: new mongoose.Types.ObjectId(),
+   //    name: {
+   //       firstName: 'Jamie',
+   //       lastName: 'Munro'
+   //    },
+   //    biography: 'Jamie is the author of ASP.NET MVC 5 with Bootstrap and Knockout.js.',
+   //    twitter: 'https://twitter.com/endyourif',
+   //    facebook: 'https://www.facebook.com/End-Your-If-194251957252562/'
+   // });
+
+   // jamieAuthor.save(function (err) {
+   //    if (err) throw err;
+
+   //    console.log('Author successfully saved.');
+
+   //    var mvcBook = new Book({
+   //       _id: new mongoose.Types.ObjectId(),
+   //       title: 'ASP.NET MVC 5 with Bootstrap and Knockout.js',
+   //       author: jamieAuthor._id,
+   //       ratings: [{
+   //          summary: 'Great read'
+   //       }]
+   //    });
+
+   //    mvcBook.save(function (err) {
+   //       if (err) throw err;
+
+   //       console.log('Book successfully saved.');
+   //    });
+
+   //    var knockoutBook = new Book({
+   //       _id: new mongoose.Types.ObjectId(),
+   //       title: 'Knockout.js: Building Dynamic Client-Side Web Applications',
+   //       author: jamieAuthor._id
+   //    });
+
+   //    knockoutBook.save(function (err) {
+   //       if (err) throw err;
+
+   //       console.log('Book successfully saved.');
+   //    });
+   // });
+
+   Book.find({
+      title: /mvc/i
+   }).sort('-created')
+      .limit(5)
+      .exec(function (err, books) {
+         if (err) throw err;
+
+         console.log(books);
+      });
+
+   Author.findById('5fc122e349f28b05ef5046b9', function (err, author) {
+      if (err) throw err;
+
+      author.linkedin = 'https://www.linkedin.com/in/jamie-munro-8064ba1a/';
+
+      author.save(function (err) {
+         if (err) throw err;
+
+         console.log('Author updated successfully');
+      });
+   });
+
+   Author.findByIdAndUpdate('59b31406beefa1082819e72f', { linkedin: 'https://www.linkedin.com/in/jamie-munro-8064ba1a/' }, function (err, author) {
+      if (err) throw err;
+
+      console.log(author);
+   });
+
+});
